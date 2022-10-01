630306499bab3e39bea0b88168bfbdd049592f83
im-shubhamsharma <im_shubhamsharma@outlook.com>
2022-10-01 21:33:45 +0530

    initial project setup

---
+++ src/components/EdenUse.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EdenUse = () => {
+  return (
+    <div>EdenUse</div>
+  )
+}
+
+export default EdenUse
---
+++ src/components/Progrees.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Progrees = () => {
+  return (
+    <div>Progrees</div>
+  )
+}
+
+export default Progrees
